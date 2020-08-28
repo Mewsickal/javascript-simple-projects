b22e59b80dd9514f59dbab85f496574295caef59
Milda <laiminga.milda@gmail.com>
2020-08-28 14:27:21 +0300

    Add option to mark new fields

--- tik-tak-toe/scripts.js
+++ tik-tak-toe/scripts.js
@@ -2,12 +2,25 @@ const boardWidth = 3;
 
 const gameBoard = (() => {
     let board = ['', 'x', '', '', '', 'o', '', 'x', ''];
+    let fields = [];
     const getBoard = () => board;
+    const onFieldClicked = (e) => {
+        if (e.target.textContent === '') {
+            e.target.textContent = 'x';
+        }
+    };
     const addDiv = (boardSize, index, container) => {
         const content = document.createElement('div');
+        content.id = index;
         content.classList.add('board-el');
-        content.textContent = board[index];
         container.appendChild(content);
+        fields.push(content);
+        content.addEventListener('click', onFieldClicked);
+    };
+    const fill = () => {
+        fields.forEach(element => {
+            element.textContent = board[element.id];
+        });
     };
     const create = () => {
         let boardSize = boardWidth * boardWidth;
@@ -15,14 +28,15 @@ const gameBoard = (() => {
             const container = document.querySelector("#container");
             addDiv(boardSize, index, container);
         }
+        fill();
     };
     return { create };
 })();
 
-const game = (() => {
+const player = () => {
     const start = () => {
     };
     return { start };
-})();
+};
 
 gameBoard.create();
