931b9499de82b97e09aec7794a8f748f41cb624e
Milda <laiminga.milda@gmail.com>
2020-08-28 16:49:32 +0300

    Add second player and game reset

--- tik-tak-toe/scripts.js
+++ tik-tak-toe/scripts.js
@@ -1,26 +1,11 @@
 const boardWidth = 3;
 
 const gameBoard = (() => {
-    let board = ['', 'x', '', '', '', 'o', '', 'x', ''];
-    let fields = [];
-    const getBoard = () => board;
-    const onFieldClicked = (e) => {
-        if (e.target.textContent === '') {
-            e.target.textContent = 'x';
-        }
-    };
     const addDiv = (boardSize, index, container) => {
         const content = document.createElement('div');
         content.id = index;
         content.classList.add('board-el');
         container.appendChild(content);
-        fields.push(content);
-        content.addEventListener('click', onFieldClicked);
-    };
-    const fill = () => {
-        fields.forEach(element => {
-            element.textContent = board[element.id];
-        });
     };
     const create = () => {
         let boardSize = boardWidth * boardWidth;
@@ -28,15 +13,64 @@ const gameBoard = (() => {
             const container = document.querySelector("#container");
             addDiv(boardSize, index, container);
         }
-        fill();
     };
     return { create };
 })();
 
-const player = () => {
-    const start = () => {
-    };
-    return { start };
+const Player = (s) => {
+    let symbol = s;
+    const getSymbol = () => symbol;
+    return { getSymbol };
 };
 
-gameBoard.create();
+const game = (() => {
+    let fields = [];
+    let player1;
+    let player2;
+    let activePlayer;
+    let winner;
+    const changeActivePlayer = () => {
+        activePlayer = (activePlayer === player1) ? player2 : player1;
+    };
+    const getGameStatus = () => {
+        let gameOver = false;
+        let winner;
+        if (fields.every(el => el.textContent != '')) {
+            gameOver = true;
+            winner = null;
+        };
+        fields.sp
+
+        return { gameOver, winner };
+    };
+    const reset = () => {
+        fields.forEach((element) => {
+            element.textContent = '';
+        });
+        activePlayer = player1;
+        winner = null;
+    };
+    const onFieldClicked = (e) => {
+        if (e.target.textContent === '') {
+            e.target.textContent = activePlayer.getSymbol();
+            changeActivePlayer();
+            let gameStatus = getGameStatus();
+            if (gameStatus.gameOver) {
+                reset();
+            }
+        }
+    };
+    const start = () => {
+        gameBoard.create();
+        fields = Array.from(document.querySelectorAll('.board-el'));
+        fields.forEach((element) => {
+            element.addEventListener('click', onFieldClicked);
+        });
+        player1 = Player('x');
+        player2 = Player('o');
+        activePlayer = player1;
+    };
+    return { start };
+})();
+
+game.start();
